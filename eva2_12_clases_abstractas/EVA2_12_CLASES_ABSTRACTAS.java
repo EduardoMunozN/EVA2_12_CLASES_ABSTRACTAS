37edb03dd16df174c263c69f39e6b3c88de69443
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-03-18 20:58:16 -0600

    Add files via upload

---
+++ eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        //NO SE PUEDEN CREAR OBJETOS DE CLASES ABSTRACTAS
+        //PruebaAbstracta pAbs = new PruebaAbstracta ();
+    }
+}
+class Prueba{
+    
+}
+
+abstract class PruebaAbstracta{
+    
+}
